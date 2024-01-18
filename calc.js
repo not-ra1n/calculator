aa6740cec6b99840d5465f067b956da1f1adf5b4
not-ra1n <142693849+not-ra1n@users.noreply.github.com>
2024-01-18 05:48:29 -0500

    To use other operations such as -, *, or /

--- calc.js
+++ calc.js
@@ -79,7 +79,7 @@ const clear = document.querySelector('#clear');
 
 clear.addEventListener('click', () => {
     valuesOne = [];
-    result = undefined;result  
+    result = undefined;  
     input.textContent = valuesOne;
     console.log(valuesOne);
 });
@@ -184,7 +184,6 @@ addition.addEventListener('click', () => {
             // let keyboard = valuesTwo.indexOf('=');
             // let mouse = valuesTwo.splice(keyboard, 1)
             valuesTwo = valuesTwo.splice(0, -1)
-            console.log(pad);
             let num2 = 0
     
     
@@ -214,7 +213,8 @@ addition.addEventListener('click', () => {
             });
         } else {
             const chicken = valuesOne.reduce((a, e, i) => {
-                if (e === "+")
+                if (e === "+" || e === "-"
+                || e === "*" || e === "/")
                 a.push(i);
                 return a;
             }, []);
@@ -240,6 +240,322 @@ addition.addEventListener('click', () => {
         console.log(nextIndex)
         if (turkey == undefined) {
             num2 = 0;
+        } else {
+            num2 = valuesOne.slice(turkey, -1)
+            console.log(num2)
+            num2 = num2.join("");
+            console.log(num2);
+            Number(num2);
+            console.log(num2);
+        }
+        
+        result = operate(num1, sign, num2)
+        console.log(result);
+        input.textContent = result;
+        }
+    
+            
+        }
+    }
+    
+});
+
+const subtraction = document.querySelector('#subtraction');
+
+subtraction.addEventListener('click', () => {
+    console.log(operator);
+    lastElement = valuesOne[valuesOne.length -1];
+    secondLastElement = valuesOne[valuesOne.length -2];
+    if (lastElement === "-" && secondLastElement === "-") {
+        valuesOne.pop();
+        console.log(valuesOne);
+        console.log(result);
+        input.textContent = result;
+    } else {
+        if (operator !== undefined) {
+            if (operator == "+") sign = "+";
+            if (operator == "-") sign = "-";
+            if (operator == "*") sign = "*";
+            if (operator == "/") sign = "/";
+            
+        const last = valuesOne[valuesOne.length - 2];
+        console.log(last);
+        if (last === '=') {
+            num1 = result;
+            console.log(num1);
+            console.log(last);
+            potato = valuesOne.indexOf(last);
+            let tomato = valuesOne.splice(potato, 1)
+            console.log(tomato);
+            // let keyboard = valuesTwo.indexOf('=');
+            // let mouse = valuesTwo.splice(keyboard, 1)
+            valuesTwo = valuesTwo.splice(0, -1)
+            let num2 = 0
+    
+    
+            buttons.forEach((button) => {
+                button.addEventListener('click', () =>{
+                    valuesTwo.push(button.textContent);
+                    console.log(valuesTwo);
+                    input.textContent = valuesTwo;
+                });
+            });
+            equal.addEventListener('click', () => {
+                console.log(valuesTwo);
+                indexOfOperator = valuesOne.lastIndexOf(operator) // you would have this already declared but we're adding it again just for readability sake
+                console.log(indexOfOperator);
+                indexOfEquals = valuesTwo.indexOf('=');
+                console.log(indexOfEquals);
+                nextIndex = indexOfOperator + 1
+                num2 = valuesTwo.slice(0, indexOfEquals)
+                num2 = num2.join("");
+                Number(num2);
+                console.log(num2);
+                console.log(operator);
+                console.log(operate);
+                result = operate(num1, operator, num2)
+                console.log(result);
+                input.textContent = result;
+            });
+        } else {
+            const chicken = valuesOne.reduce((a, e, i) => {
+                if (e === "-" || e === "+"
+                || e === "*" || e === "/")
+                a.push(i);
+                return a;
+            }, []);
+            console.log(chicken);
+    
+               let turkey = (chicken[chicken.length-2]);
+                console.log(turkey);       
+            
+    
+    
+        indexOfOperator = valuesOne.lastIndexOf(sign);
+        console.log(indexOfOperator)
+        if (result != undefined) {
+            num1 = result;
+        } else {
+            num1 = valuesOne.slice(0, indexOfOperator)
+            console.log(num1);
+            num1 = num1.join("");
+            Number(num1);
+        }
+        console.log(num1)
+        nextIndex = indexOfOperator + 1;
+        console.log(nextIndex)
+        if (turkey == undefined) {
+            num2 = 0;
+        } else {
+            num2 = valuesOne.slice(turkey, -1)
+            console.log(num2)
+            num2 = num2.join("");
+            console.log(num2);
+            Number(num2);
+            console.log(num2);
+        }
+        
+        result = operate(num1, sign, num2)
+        console.log(result);
+        input.textContent = result;
+        }
+    
+            
+        }
+    }
+    
+});
+
+const multiplication = document.querySelector('#multiplication');
+
+multiplication.addEventListener('click', () => {
+    console.log(operator);
+    lastElement = valuesOne[valuesOne.length -1];
+    secondLastElement = valuesOne[valuesOne.length -2];
+    if (lastElement === "*" && secondLastElement === "*") {
+        valuesOne.pop();
+        console.log(valuesOne);
+        console.log(result);
+        input.textContent = result;
+    } else {
+        if (operator !== undefined) {
+            if (operator == "+") sign = "+";
+            if (operator == "-") sign = "-";
+            if (operator == "*") sign = "*";
+            if (operator == "/") sign = "/";
+            
+        const last = valuesOne[valuesOne.length - 2];
+        console.log(last);
+        if (last === '=') {
+            num1 = result;
+            console.log(num1);
+            console.log(last);
+            potato = valuesOne.indexOf(last);
+            let tomato = valuesOne.splice(potato, 1)
+            console.log(tomato);
+            // let keyboard = valuesTwo.indexOf('=');
+            // let mouse = valuesTwo.splice(keyboard, 1)
+            valuesTwo = valuesTwo.splice(0, -1)
+            let num2 = 0
+    
+    
+            buttons.forEach((button) => {
+                button.addEventListener('click', () =>{
+                    valuesTwo.push(button.textContent);
+                    console.log(valuesTwo);
+                    input.textContent = valuesTwo;
+                });
+            });
+            equal.addEventListener('click', () => {
+                console.log(valuesTwo);
+                indexOfOperator = valuesOne.lastIndexOf(operator) // you would have this already declared but we're adding it again just for readability sake
+                console.log(indexOfOperator);
+                indexOfEquals = valuesTwo.indexOf('=');
+                console.log(indexOfEquals);
+                nextIndex = indexOfOperator + 1
+                num2 = valuesTwo.slice(0, indexOfEquals)
+                num2 = num2.join("");
+                Number(num2);
+                console.log(num2);
+                console.log(operator);
+                console.log(operate);
+                result = operate(num1, operator, num2)
+                console.log(result);
+                input.textContent = result;
+            });
+        } else {
+            const chicken = valuesOne.reduce((a, e, i) => {
+                if (e === "*")
+                a.push(i);
+                return a;
+            }, []);
+            console.log(chicken);
+    
+               let turkey = (chicken[chicken.length-2]);
+                console.log(turkey);       
+            
+    
+    
+        indexOfOperator = valuesOne.lastIndexOf(sign);
+        console.log(indexOfOperator)
+        if (result != undefined) {
+            num1 = result;
+        } else {
+            num1 = valuesOne.slice(0, indexOfOperator)
+            console.log(num1);
+            num1 = num1.join("");
+            Number(num1);
+        }
+        console.log(num1)
+        nextIndex = indexOfOperator + 1;
+        console.log(nextIndex)
+        if (turkey == undefined) {
+            num2 = 1;
+        } else {
+            num2 = valuesOne.slice(turkey, -1)
+            console.log(num2)
+            num2 = num2.join("");
+            console.log(num2);
+            Number(num2);
+            console.log(num2);
+        }
+        
+        result = operate(num1, sign, num2)
+        console.log(result);
+        input.textContent = result;
+        }
+    
+            
+        }
+    }
+    
+});
+
+const division = document.querySelector('#division');
+
+division.addEventListener('click', () => {
+    console.log(operator);
+    lastElement = valuesOne[valuesOne.length -1];
+    secondLastElement = valuesOne[valuesOne.length -2];
+    if (lastElement === "/" && secondLastElement === "/") {
+        valuesOne.pop();
+        console.log(valuesOne);
+        console.log(result);
+        input.textContent = result;
+    } else {
+        if (operator !== undefined) {
+            if (operator == "+") sign = "+";
+            if (operator == "-") sign = "-";
+            if (operator == "*") sign = "*";
+            if (operator == "/") sign = "/";
+            
+        const last = valuesOne[valuesOne.length - 2];
+        console.log(last);
+        if (last === '=') {
+            num1 = result;
+            console.log(num1);
+            console.log(last);
+            potato = valuesOne.indexOf(last);
+            let tomato = valuesOne.splice(potato, 1)
+            console.log(tomato);
+            // let keyboard = valuesTwo.indexOf('=');
+            // let mouse = valuesTwo.splice(keyboard, 1)
+            valuesTwo = valuesTwo.splice(0, -1)
+            let num2 = 0
+    
+    
+            buttons.forEach((button) => {
+                button.addEventListener('click', () =>{
+                    valuesTwo.push(button.textContent);
+                    console.log(valuesTwo);
+                    input.textContent = valuesTwo;
+                });
+            });
+            equal.addEventListener('click', () => {
+                console.log(valuesTwo);
+                indexOfOperator = valuesOne.lastIndexOf(operator) // you would have this already declared but we're adding it again just for readability sake
+                console.log(indexOfOperator);
+                indexOfEquals = valuesTwo.indexOf('=');
+                console.log(indexOfEquals);
+                nextIndex = indexOfOperator + 1
+                num2 = valuesTwo.slice(0, indexOfEquals)
+                num2 = num2.join("");
+                Number(num2);
+                console.log(num2);
+                console.log(operator);
+                console.log(operate);
+                result = operate(num1, operator, num2)
+                console.log(result);
+                input.textContent = result;
+            });
+        } else {
+            const chicken = valuesOne.reduce((a, e, i) => {
+                if (e === "/")
+                a.push(i);
+                return a;
+            }, []);
+            console.log(chicken);
+    
+               let turkey = (chicken[chicken.length-2]);
+                console.log(turkey);       
+            
+    
+    
+        indexOfOperator = valuesOne.lastIndexOf(sign);
+        console.log(indexOfOperator)
+        if (result != undefined) {
+            num1 = result;
+        } else {
+            num1 = valuesOne.slice(0, indexOfOperator)
+            console.log(num1);
+            num1 = num1.join("");
+            Number(num1);
+        }
+        console.log(num1)
+        nextIndex = indexOfOperator + 1;
+        console.log(nextIndex)
+        if (turkey == undefined) {
+            num2 = 1;
         } else {
             num2 = valuesOne.slice(turkey, -1)
             console.log(num2)
